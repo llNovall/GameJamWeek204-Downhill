7aa41be29b7d2cabd1a6d8e17c4a2a5fddf2e2bd
llNovall <25434267+llNovall@users.noreply.github.com>
2021-06-09 18:40:35 +0200

    Updated PlayerIdentifier script

--- Assets/Scripts/Player/PlayerIdentifier.cs
+++ Assets/Scripts/Player/PlayerIdentifier.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class PlayerIdentifier : MonoBehaviour
 {
     public static PlayerIdentifier Current;
+    public static PlayerEnergy PlayerEnergy;
     private void Awake()
     {
         Current = this;
+    }
+
+    private void Start()
+    {
+        PlayerEnergy = GetComponent<PlayerEnergy>();
     }
 }
